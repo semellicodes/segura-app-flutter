740d3a5b1c0f255100e1d34b4fd7faa9a19141bd
Paula Semelli Araujo do Amaral <paulaufra2023@gmail.com>
2026-03-11 00:07:51 -0300

    adicionando widget topico.dart

---
+++ lib/widgets/topico.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class Topico extends StatelessWidget {
+  final String titulo;
+  final String descricao;
+
+  const Topico({super.key, required this.titulo, required this.descricao});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12.0),
+      child: Text.rich(
+        TextSpan(
+          style: const TextStyle(
+            fontSize: 16,
+            color: Colors.black87,
+          ), // Estilo geral
+          children: [
+            TextSpan(
+              text: '• $titulo ',
+              style: const TextStyle(fontWeight: FontWeight.bold),
+            ),
+            TextSpan(text: descricao),
+          ],
+        ),
+      ),
+    );
+  }
+}

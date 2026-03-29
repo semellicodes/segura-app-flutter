f7f29fa188b6be7b8b2367483f9a0053e96ed1e3
Paula Semelli Araujo do Amaral <paulaufra2023@gmail.com>
2026-03-29 00:41:07 -0300

    UI: Atualiza cores do tema para tons roxos mais ricos e premium

--- lib/theme/app_colors.dart
+++ lib/theme/app_colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF512DA8);
+  static const Color primary = Color(0xFF311B92);
 
-  static const Color secondary = Color(0xFFD1C4E9);
+  static const Color secondary = Color(0xFF7E57C2);
 
   static const Color background = Color(0xFFFAFAFA);
 
-  static const Color ajuda = Color(0xFF673AB7);
+  static const Color ajuda = Color(0xFF512DA8);
 
   static const Color alertaVermelho = Color(0xFFD32F2F);
 }
